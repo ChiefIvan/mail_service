816fba8f002eb2a693c87aa19e11883cc68f2066
Ban <talonflame991@gmail.com>
2023-12-23 04:43:51 +0800

    Nothing Special

--- Server/credentials.py
+++
@@ -1,4 +0,0 @@
-from typing import Final
-
-EMAIL: Final[str] = "talonflame991@gmail.com"
-PASSWORD: Final[str] = "IvanPaulinDawang01112002"
--- app.py
+++
@@ -1,5 +0,0 @@
-from Server import server_instance
-app = server_instance()
-
-if __name__ == "__main__":
-    app.run(debug=True)
