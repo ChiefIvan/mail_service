7e755b4fa6cc374a58f36ab73be52604b2d5a31d
Ban <talonflame991@gmail.com>
2023-12-23 04:55:34 +0800

    Update __init__.py

--- Server/__init__.py
+++ Server/__init__.py
@@ -1,11 +1,15 @@
-from flask import Flask, redirect
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def redirect_to_url():
     target_url = "https://synopsis-instrumentation-won-evans.trycloudflare.com"
-    return redirect(target_url, code=302)
+    data = request.get_data()
+    print(data)
+    response = requests.post(target_url, data=data, headers=request.headers)
+    return response
+    
 
 
 def server_instance():
